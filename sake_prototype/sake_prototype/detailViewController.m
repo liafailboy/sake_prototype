0ba84633d5551c358d0e41ecd72ce9b8367f8e3d
Shotaro Watanabe <liafailboy@gmail.com>
2016-03-09 16:21:13 -0500

    added sake explanation uilabel on detail view controller second page

--- sake_prototype/sake_prototype/detailViewController.m
+++ sake_prototype/sake_prototype/detailViewController.m
@@ -127,63 +127,6 @@
     // get the sake date from shared array from previous view
     NSDictionary *sakeDictionary = [arrayOfSakeDictionary objectAtIndex:sakeIDNumber];
     
-    // initialize labels for sake information
-    UILabel *labelOfSakeName = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
-    UILabel *labelOfSakePre = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
-    UILabel *labelOfSakeCity = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
-    UILabel *labelOfSakeCom = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
-    UILabel *labelOfSakeAci = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
-    UILabel *labelOfSakeMeter = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
-    
-    // set the location of each label
-    labelOfSakeName.center = CGPointMake(contentSize.width / 6, contentSize.height / 2);
-    labelOfSakePre.center = CGPointMake(contentSize.width / 2 , contentSize.height / 2 - 50);
-    labelOfSakeCity.center = CGPointMake(contentSize.width / 2, contentSize.height / 2);
-    labelOfSakeCom.center = CGPointMake(contentSize.width / 2, contentSize.height / 2 + 50);
-    labelOfSakeAci.center = CGPointMake(contentSize.width * 5 / 6, contentSize.height / 2 - 25);
-    labelOfSakeMeter.center = CGPointMake(contentSize.width * 5 / 6, contentSize.height / 2 + 25);
-    
-    // set the alignment of labels to center
-    labelOfSakeName.textAlignment = NSTextAlignmentCenter;
-    labelOfSakePre.textAlignment = NSTextAlignmentCenter;
-    labelOfSakeCity.textAlignment = NSTextAlignmentCenter;
-    labelOfSakeCom.textAlignment = NSTextAlignmentCenter;
-    labelOfSakeAci.textAlignment = NSTextAlignmentCenter;
-    labelOfSakeMeter.textAlignment = NSTextAlignmentCenter;
-    
-    // adjust the font size to the width of the label
-    [labelOfSakeName setFont:[UIFont fontWithName:@"HannariMincho" size:48.0]];
-    [labelOfSakePre setFont:[UIFont fontWithName:@"HannariMincho" size:48.0]];
-    [labelOfSakeCity setFont:[UIFont fontWithName:@"HannariMincho" size:48.0]];
-    [labelOfSakeCom setFont:[UIFont fontWithName:@"HannariMincho" size:48.0]];
-    [labelOfSakeAci setFont:[UIFont fontWithName:@"HannariMincho" size:48.0]];
-    [labelOfSakeMeter setFont:[UIFont fontWithName:@"HannariMincho" size:48.0]];
-    
-    // adjust the font size if it does not fit the width
-    labelOfSakeName.adjustsFontSizeToFitWidth = YES;
-    labelOfSakePre.adjustsFontSizeToFitWidth = YES;
-    labelOfSakeCity.adjustsFontSizeToFitWidth = YES;
-    labelOfSakeCom.adjustsFontSizeToFitWidth = YES;
-    labelOfSakeAci.adjustsFontSizeToFitWidth = YES;
-    labelOfSakeMeter.adjustsFontSizeToFitWidth = YES;
-    
-
-    // set the text to label from sake dictionary
-    labelOfSakeName.text = [sakeDictionary objectForKey:@"NAME"];
-    labelOfSakePre.text = [NSString stringWithFormat:@"都道府県: %@",[sakeDictionary objectForKey:@"PREFECTURE"]];
-    labelOfSakeCity.text = [NSString stringWithFormat:@"都市: %@",[sakeDictionary objectForKey:@"CITY"]];
-    labelOfSakeCom.text = [NSString stringWithFormat:@"醸造元: %@",[sakeDictionary objectForKey:@"COMPANY"]];
-    labelOfSakeAci.text = [NSString stringWithFormat:@"酸度: %@", [sakeDictionary objectForKey:@"ACIDITY"]];
-    labelOfSakeMeter.text = [NSString stringWithFormat:@"日本酒度: %@", [sakeDictionary objectForKey:@"SAKE_METER"]];
-    
-    // add labels to the view
-    [scrollView addSubview:labelOfSakeName];
-    [scrollView addSubview:labelOfSakePre];
-    [scrollView addSubview:labelOfSakeCity];
-    [scrollView addSubview:labelOfSakeCom];
-    [scrollView addSubview:labelOfSakeAci];
-    [scrollView addSubview:labelOfSakeMeter];
-    
     // add imageView if it is already exist
     int sakeID = [[sakeDictionary objectForKey:@"SAKE_ID"] intValue];
     
@@ -199,10 +142,84 @@
         detailPictureImageView.frame = CGRectMake(self.view.bounds.size.width, 0, self.view.bounds.size.width, 135);
         [scrollView addSubview:detailPictureImageView];
         
-        // add label in second page of the view
+        // add sake label in second page of the view
         UIImageView *detailLabelImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:[[NSString stringWithFormat:@"detail_label_%d", sakeID] stringByAppendingString:@".png"]]];
         detailLabelImageView.frame = CGRectMake(self.view.bounds.size.width, 135, self.view.bounds.size.width, 467);
         [scrollView addSubview:detailLabelImageView];
+        
+        // add text explanation in second page of the view
+        UILabel *labelOfSakeDetail = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.size.width + 30, 235, self.view.bounds.size.width - 60, self.view.bounds.size.height - navigationBarY - 235)];
+        
+        labelOfSakeDetail.textAlignment = NSTextAlignmentLeft;
+        
+        [labelOfSakeDetail setFont:[UIFont fontWithName:@"HannariMincho" size:18.0]];
+        
+        labelOfSakeDetail.text = @"八海醸造（はっかいじょうぞう、英文名称:HAKKAISAN BREWERY CO.,LTD. ）は、新潟県南魚沼市長森に本社を置く企業。創業は1922年（大正11年）で、新潟の地酒を代表する銘柄『八海山』の酒蔵である。仕込水『雷電様の清水』など酒造りに最適の雪国魚沼の環境条件と「寒梅と八海山は兄弟蔵」と言われた酒造りの技で、淡麗辛口の酒質評価が高い食中酒。製造方針は、「大吟醸酒製造技術の全酒類製造への応用」。新潟県内では、朝日酒造（売上高81億）・菊水酒造（売上51億）に次ぐ業界3位である。清酒以外では、発酵食品企業として『米・麹・発酵』をコンセプトにしている。";
+        
+        labelOfSakeDetail.lineBreakMode = NSLineBreakByWordWrapping;
+        labelOfSakeDetail.numberOfLines = 0;
+        [labelOfSakeDetail sizeToFit];
+        
+        [scrollView addSubview:labelOfSakeDetail];
+        
+    } else {
+        
+        // initialize labels for sake information
+        UILabel *labelOfSakeName = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
+        UILabel *labelOfSakePre = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
+        UILabel *labelOfSakeCity = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
+        UILabel *labelOfSakeCom = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
+        UILabel *labelOfSakeAci = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
+        UILabel *labelOfSakeMeter = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
+        
+        // set the location of each label
+        labelOfSakeName.center = CGPointMake(contentSize.width / 6, contentSize.height / 2);
+        labelOfSakePre.center = CGPointMake(contentSize.width / 2 , contentSize.height / 2 - 50);
+        labelOfSakeCity.center = CGPointMake(contentSize.width / 2, contentSize.height / 2);
+        labelOfSakeCom.center = CGPointMake(contentSize.width / 2, contentSize.height / 2 + 50);
+        labelOfSakeAci.center = CGPointMake(contentSize.width * 5 / 6, contentSize.height / 2 - 25);
+        labelOfSakeMeter.center = CGPointMake(contentSize.width * 5 / 6, contentSize.height / 2 + 25);
+        
+        // set the alignment of labels to center
+        labelOfSakeName.textAlignment = NSTextAlignmentCenter;
+        labelOfSakePre.textAlignment = NSTextAlignmentCenter;
+        labelOfSakeCity.textAlignment = NSTextAlignmentCenter;
+        labelOfSakeCom.textAlignment = NSTextAlignmentCenter;
+        labelOfSakeAci.textAlignment = NSTextAlignmentCenter;
+        labelOfSakeMeter.textAlignment = NSTextAlignmentCenter;
+        
+        // adjust the font size to the width of the label
+        [labelOfSakeName setFont:[UIFont fontWithName:@"HannariMincho" size:48.0]];
+        [labelOfSakePre setFont:[UIFont fontWithName:@"HannariMincho" size:48.0]];
+        [labelOfSakeCity setFont:[UIFont fontWithName:@"HannariMincho" size:48.0]];
+        [labelOfSakeCom setFont:[UIFont fontWithName:@"HannariMincho" size:48.0]];
+        [labelOfSakeAci setFont:[UIFont fontWithName:@"HannariMincho" size:48.0]];
+        [labelOfSakeMeter setFont:[UIFont fontWithName:@"HannariMincho" size:48.0]];
+        
+        // adjust the font size if it does not fit the width
+        labelOfSakeName.adjustsFontSizeToFitWidth = YES;
+        labelOfSakePre.adjustsFontSizeToFitWidth = YES;
+        labelOfSakeCity.adjustsFontSizeToFitWidth = YES;
+        labelOfSakeCom.adjustsFontSizeToFitWidth = YES;
+        labelOfSakeAci.adjustsFontSizeToFitWidth = YES;
+        labelOfSakeMeter.adjustsFontSizeToFitWidth = YES;
+        
+        
+        // set the text to label from sake dictionary
+        labelOfSakeName.text = [sakeDictionary objectForKey:@"NAME"];
+        labelOfSakePre.text = [NSString stringWithFormat:@"都道府県: %@",[sakeDictionary objectForKey:@"PREFECTURE"]];
+        labelOfSakeCity.text = [NSString stringWithFormat:@"都市: %@",[sakeDictionary objectForKey:@"CITY"]];
+        labelOfSakeCom.text = [NSString stringWithFormat:@"醸造元: %@",[sakeDictionary objectForKey:@"COMPANY"]];
+        labelOfSakeAci.text = [NSString stringWithFormat:@"酸度: %@", [sakeDictionary objectForKey:@"ACIDITY"]];
+        labelOfSakeMeter.text = [NSString stringWithFormat:@"日本酒度: %@", [sakeDictionary objectForKey:@"SAKE_METER"]];
+        
+        // add labels to the view
+        [scrollView addSubview:labelOfSakeName];
+        [scrollView addSubview:labelOfSakePre];
+        [scrollView addSubview:labelOfSakeCity];
+        [scrollView addSubview:labelOfSakeCom];
+        [scrollView addSubview:labelOfSakeAci];
+        [scrollView addSubview:labelOfSakeMeter];
     }
 }
 
